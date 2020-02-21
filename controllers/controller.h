249081f1247d197d8e464b3985cf3d62aede110c
red1bluelost <msweston12@cox.net>
2020-02-20 19:40:57 -0500

    Creates controller parent class.

---
+++ controllers/controller.h
@@ -0,0 +1,22 @@
+/* File:   controllers.h
+ * Author: Micah Weston
+ * Info:   Creates parent class for the different controllers
+ */
+
+#ifndef __CONTROLLERS_H__
+#define __CONTROLLERS_H__
+
+
+#include "../communication/game_protocol.h"
+
+class controller {
+	//privatize default constructor to prevent use
+	controller();
+	
+	//available to the derived controllers
+	protected:
+	const CompID CID;
+	controller(CompID cid) : CID(cid);
+}
+
+#endif
